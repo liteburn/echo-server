6acc2807c851d8fb53d6ee0fcfd048920e6e594f
markoburda <burda@ucu.edu.ua>
2020-04-10 13:41:14 +0300

    Barebones code

---
+++ client.cpp
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include<string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main(int argc,char **argv)
+{
+    int cli_sock,n;
+    char send[100];
+    char recv[100];
+    struct sockaddr_in serv_addr;
+ 
+    cli_sock=socket(AF_INET,SOCK_STREAM,0);
+    bzero(&serv_addr,sizeof serv_addr);
+ 
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_port=htons(22000);
+ 
+    inet_pton(AF_INET,"127.0.0.1",&(serv_addr.sin_addr));
+ 
+    connect(cli_sock,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
+ 
+    while(1)
+    {
+        bzero(send, 100);
+        bzero(recv, 100);
+
+        fgets(send,100,stdin);
+        write(cli_sock,send,strlen(send)+1);
+        read(cli_sock,recv,100);
+
+        printf("\n%s",recv);
+    }
+ 
+}
---
+++ server.cpp
@@ -0,0 +1,31 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include<string.h>
+#include <unistd.h>
+
+int main()
+{
+    int listen_fd, comm_fd;
+    struct sockaddr_in serv_addr;
+    char input[100];
+ 
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero( &serv_addr, sizeof(serv_addr));
+ 
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    serv_addr.sin_port = htons(22000);
+ 
+    bind(listen_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    listen(listen_fd, 10);
+    comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
+ 
+    while(1)
+    {
+        bzero( input, 100);
+        read(comm_fd,input,100);
+        write(comm_fd, input, strlen(input)+1);
+    }
+}
